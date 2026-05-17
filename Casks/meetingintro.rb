c989d5141a1a42ea35fab05cbce46c4ddbc73ed3
Oluyinka Oginni <oluyinkaoginni@Oluyinkas-MacBook-Pro.local>
2026-05-17 02:37:21 -0700

    meetingintro 1.0.0

---
+++ Casks/meetingintro.rb
@@ -0,0 +1,19 @@
+cask "meetingintro" do
+  version "1.0.0"
+  sha256 "1ededd0db583d4501b988462a2d97945c16dc0d4478d4fb63c0db6ea7870faef"
+
+  url "https://github.com/templegit9/MeetingIntro/releases/download/v#{version}/MeetingIntro-#{version}.zip"
+  name "MeetingIntro"
+  desc "Menu bar app that plays a countdown overlay before calendar meetings"
+  homepage "https://github.com/templegit9/MeetingIntro"
+
+  depends_on macos: ">= :sonoma"
+
+  app "MeetingIntro.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.oluyinka.MeetingIntro.plist",
+    "~/Library/Application Support/MeetingIntro",
+    "~/Library/Caches/com.oluyinka.MeetingIntro",
+  ]
+end
