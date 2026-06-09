ed34e519b8a02f9302fa119b91d045f62bc2c967
Oluyinka Oginni <oluyinkaoginni@gmail.com>
2026-06-09 10:47:39 -0400

    meetingintro 2.5.0

--- Casks/meetingintro.rb
+++ Casks/meetingintro.rb
@@ -1,6 +1,6 @@
 cask "meetingintro" do
-  version "2.4.1"
-  sha256 "a9c50d1b5065552639ffd87906f3cad2264bb98c1e2073191e043b4d27e3f56e"
+  version "2.5.0"
+  sha256 "5a34b72b29fe303d7597733378e107c7f319d3731839eddb3b86362e5ac4842d"
 
   url "https://github.com/templegit9/MeetingIntro/releases/download/v#{version}/MeetingIntro-#{version}.zip"
   name "MeetingIntro"
